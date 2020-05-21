094f640e6dd3a4664f01e5871e5e9500aecd2679
Sagnik Bandyopadhyay <sagnikbanerjeesb@gmail.com>
2020-05-21 19:43:22 +0530

    Added about activity

--- app/src/main/java/com/sagnik/covidnotifier/MainActivity.java
+++ app/src/main/java/com/sagnik/covidnotifier/MainActivity.java
@@ -1,10 +1,14 @@
 package com.sagnik.covidnotifier;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -51,6 +55,28 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         addTextToScrollViewLayout("Loading Data...");
         LoaderManager.getInstance(this).restartLoader(LOAD_DATA, new Bundle(), this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about_button:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     @NonNull
