34b75d92542a204e462eca316d87671c5b504951
Sagnik Bandyopadhyay <sagnikbanerjeesb@gmail.com>
2020-06-11 03:19:54 +0530

    Refactored

--- app/src/main/java/com/sagnik/covidnotifier/sync/SyncWorker.java
+++ app/src/main/java/com/sagnik/covidnotifier/sync/SyncWorker.java
@@ -1,6 +1,5 @@
 package com.sagnik.covidnotifier.sync;
 
-import android.app.NotificationManager;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
@@ -13,8 +12,6 @@ import com.sagnik.covidnotifier.services.CovidCountChangeNotificationService;
 import javax.inject.Inject;
 
 public class SyncWorker extends Worker {
-    private NotificationManager mNotifyManager;
-
     @Inject
     CovidCountChangeNotificationService covidCountChangeNotificationService;
 
